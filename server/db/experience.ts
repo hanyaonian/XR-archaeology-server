a819a7b59e0d249e852a921e4828992f60fe164e
MichaelHyn <hanyaonian@outlook.com>
2024-07-09 00:31:59 +0800

    feat: experience

---
+++ server/db/experience.ts
@@ -0,0 +1,49 @@
+import type { SchemaDefExt } from "../feathers/schema";
+
+const schema: SchemaDefExt = {
+  name: { type: String, index: true },
+  briefDesc: { type: String, $editor: { props: { multiLine: true } } },
+  desc: { type: String, $editor: { props: { multiLine: true } } },
+  images: [{ type: "id", ref: "Attachment", fileType: "image" }],
+  content: [
+    {
+      heading: { type: String },
+      desc: { type: String, $editor: { props: { multiLine: true } } },
+      images: [{ type: "id", ref: "Attachment", fileType: "image" }],
+    },
+  ],
+
+  personalLink: { type: String, required: true },
+
+  language: { type: String, required: true },
+
+  order: { type: Number, default: 0, min: 0 },
+
+  businessHours: [
+    {
+      openTime: { type: String, required: true, $editor: { props: { type: "time" } } },
+      closeTime: { type: String, required: true, $editor: { props: { type: "time" } } },
+      days: [{ type: String, required: true, enum: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"] }],
+    },
+  ],
+
+  $services: {
+    services: {
+      experience: {},
+    },
+    public: {
+      experience: {
+        hooks_Auth: ["readOnlyHooks"],
+      },
+    },
+  },
+  $params: {
+    editor: {
+      headers: ["name", "order"],
+      icon: "MdEvent",
+      groupIcon: "MdEmojiEmotions",
+    },
+  },
+};
+
+export default schema;
